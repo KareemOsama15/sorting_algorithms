3b224bbfcb0be03931b0b14423186b1d410c40d8
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-18 07:35:18 +0200

    [push] project sorting_algorithms tasks[0, 1]

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that implement the bubble sort algorithm
+ * @array: the array that contain numbers
+ * @size: size of array
+ * Return: no return
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, temp, swap = 1;
+
+	for (i = 0; (i < size) && swap; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that implement the insertion sort algorithm
+ * @list: the head of doubly linked list
+ * Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list, *temp = NULL;
+
+	while (current)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+
+			if (temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+
+			temp->next->prev = temp;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
