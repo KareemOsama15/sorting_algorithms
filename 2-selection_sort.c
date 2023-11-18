34084c8ba548b7368528b550f649198de5ee67f0
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-18 07:51:31 +0200

    [push] project sorting_algorithms tasks[0, 1]

---
+++ 2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that implement the selection sort algorithm
+ * @array: the array that contain numbers
+ * @size: size of array
+ * Return: no return
+ */
+void selection_sort(int *array, size_t size)
+{
+
+}
