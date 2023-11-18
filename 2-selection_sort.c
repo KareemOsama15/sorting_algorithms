d51e2daa619bb20895904a628650f25fd4a7ca8f
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-18 21:34:17 +0200

    [push] project sorting_algorithms tasks[2]

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,23 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int minNum, temp, index;
+	int tmp;
+	size_t i, j, m;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		minNum = array[i];
+		m = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (minNum >= array[j])
-			{
-				minNum = array[j];
-				index = j;
-			}
+			if (array[m] > array[j])
+				m = j;
 		}
-		if (array[i] > minNum)
-		{
-			temp = array[index];
-			array[index] = array[i];
-			array[i] = temp;
 
+		if (i != m)
+		{
+			tmp = array[i];
+			array[i] = array[m];
+			array[m] = tmp;
 			print_array(array, size);
 		}
 	}
