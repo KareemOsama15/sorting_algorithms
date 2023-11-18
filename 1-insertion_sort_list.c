bb748ae453b9138b310cd6cc015e7eb21d449dd9
KareemOsama15 <karim.osama.saleh@gmail.com>
2023-11-18 09:15:13 +0200

    [push] project sorting_algorithms tasks[0, 1]

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -21,14 +21,14 @@ void insertion_sort_list(listint_t **list)
 				temp->next->prev = temp->prev;
 			temp->prev->next = temp->next;
 			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
+			temp->prev = temp->next->prev;
+			temp->next->prev = temp;
 
 			if (temp->prev == NULL)
 				*list = temp;
 			else
 				temp->prev->next = temp;
 
-			temp->next->prev = temp;
 			print_list(*list);
 		}
 		current = current->next;
